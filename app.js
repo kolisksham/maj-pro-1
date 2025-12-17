a1962aeb24415070debebf8da231e3b2acd8cf00
kolisksham <kolisksham@gmail.com>
2025-12-17 11:06:47 +0530

    adding files on git

---
+++ app.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = 8080
+const MONGO_URL = "mongodb://localhost:27017/yoyo";
+
+const app = express();
+
+main()
+    .then(()=>{
+        console.log("DB connection Successful!");
+    }).catch((e)=>{
+        console.log(e)
+    });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+};
+
+app.get("/", (req, res)=>{
+    res.send("Root Node, Landing Page");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server online on Port: ${PORT}`);
+});
