625c78a5f3170ae4f749a170bfffca487187bfd5
kolisksham <kolisksham@gmail.com>
2025-12-28 06:17:56 +0530

    phase one for basic routing completed.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
+const path = require("path");
+const methodOverride = require("method-override");
 
 const PORT = 8080
 const MONGO_URL = "mongodb://localhost:27017/yoyo";
 
 const app = express();
+
+
 
 main()
     .then(()=>{
@@ -18,15 +22,54 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 };
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res)=>{
     res.send("Root Node, Landing Page");
 });
 
 app.get("/listings", async (req, res)=>{
-    await Listing.find({}).then((res)=>{
-        console.log(res);
-    });
+    const allListings = await Listing.find({});
+    res.render("listings/index", { allListings });
 });
+
+app.get("/listings/new", async (req, res)=>{
+    res.render("listings/new")
+}); 
+
+app.get("/listings/:id", async (req, res)=>{
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show", { listing });
+});
+
+app.post("/listings", async (req,res)=>{
+    const newListing = new Listing (req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit", async( req, res)=>{
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit", { listing });
+});
+
+app.put("/listings/:id", async( req, res)=>{
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect("/listings");
+});
+
+app.delete("/listings/:id", async( req, res)=>{
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+});
+
 
 
 
